1060577be780e367601edff0860bd6321b358a2a
pashtet1092 <pashanpmrp200431@gmail.com>
2023-03-29 14:30:06 +0300

    Main

---
+++ Lab1/main.py
@@ -0,0 +1,15 @@
+import Task1
+import Task2
+import Task3
+
+
+print("Task1:\n",end="")
+Task1.say_hello()
+
+print("Task2:\n",end="")
+print(Task2.simple_calculator())
+
+print("Task3:\n",end="")
+Task3.show_even_list()
+
+
