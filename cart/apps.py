62699015a1a01d1b9e8483267ab8aeb08cb72b27
Pavel Pavlyutkin <pasjanpmrp200431@gmail.com>
2023-06-06 19:44:40 +0300

    adding my cart microservice

---
+++ cart/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+
+class CartConfig(AppConfig):
+    """
+    cart(vet cart) configurations
+    """
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'cart'
